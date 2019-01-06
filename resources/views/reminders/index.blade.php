94b5ea7721c336c90cd21e04a4cfa4c797bca12b
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2019-01-05 20:18:28 -0800

    Reminder page updates

--- resources/views/reminders/index.blade.php
+++ resources/views/reminders/index.blade.php
@@ -6,40 +6,42 @@
     @include('reminders._form')
 
     @if ($reminders->count())
-        <table class="table table-responsive mt-4">
-            <thead>
-                <tr>
-                    <th>Reminder</th>
-                    <th>Frequency</th>
-                    <th>Day</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Run Once?</th>
-                    <th>&nbsp;</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @foreach ($reminders as $reminder)
+        <div class="table-responsive mt-4">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $reminder->body }}</td>
-                        <td>{{ $reminder->frequency }}</td>
-                        <td>{{ $reminder->day ?: '-'}}</td>
-                        <td>{{ $reminder->date ?: '-' }}</td>
-                        <td>{{ $reminder->time }}</td>
-                        <td>{{ $reminder->run_once ? 'Yes' : 'No' }}</td>
-                        <td>
-                            <form action='/reminders/{{ $reminder->id }}' method="POST">
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" class="btn-link text-danger">Delete</button>
-                            </form>
-                        </td>
+                        <th>Reminder</th>
+                        <th>Frequency</th>
+                        <th>Day</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Run Once?</th>
+                        <th>&nbsp;</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    @foreach ($reminders as $reminder)
+                        <tr>
+                            <td>{{ $reminder->body }}</td>
+                            <td>{{ $reminder->frequency }}</td>
+                            <td>{{ $reminder->day ?: '-'}}</td>
+                            <td>{{ $reminder->date ?: '-' }}</td>
+                            <td>{{ $reminder->time ? now()->setTimeFromTimeString($reminder->time)->format('h:i a') : '-' }}</td>
+                            <td>{{ $reminder->run_once ? 'Yes' : 'No' }}</td>
+                            <td class="text-right">
+                                <form action='/reminders/{{ $reminder->id }}' method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button type="submit" class="btn-link text-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     @else
         <hr>
 
